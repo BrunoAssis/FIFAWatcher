758a5009d1e735bf2a1b9b534e95b3c8e1214f6c
Bruno Assis <brunoassis@gmail.com>
2014-07-05 18:22:02 -0300

    TRY FOREVER.

--- main.rb
+++ main.rb
@@ -7,27 +7,30 @@ curl_service = Curl::Easy.new("https://fwctickets.fifa.com/TopsAkaCalls/Calls.as
 	curl.headers["User-Agent"] = "Chrome/35.0.1916.153"
 end
 
-begin
-  while (true)
-    curl_service.perform
+while (true)
+  begin
+    while (true)
+      curl_service.perform
 
-    json_response = JSON.parse(curl_service.body_str)
-    json_response = JSON.parse(json_response["d"]["data"])
+      json_response = JSON.parse(curl_service.body_str)
+      json_response = JSON.parse(json_response["d"]["data"])
 
-    products = json_response["BasicCodes"]["PRODUCTPRICES"]
-    products = products.select{|p| p["PRPProductId"] == "IMT62" && p["Quantity"].to_i > 1}
+      products = json_response["BasicCodes"]["PRODUCTPRICES"]
+      products = products.select{|p| p["PRPProductId"] == "IMT62" && p["Quantity"].to_i > 1}
 
-    if products.size > 0
-      puts "Tem! Enviando tweet!"
+      if products.size > 0
+        puts "Tem! Enviando tweet!"
 
-      `t update "@BrunoAssis @KarenKoritiak o devorador de mundos acordou as #{Time.now.to_s}"`
-    else
-      puts "Não tem :("
+        `t update "@BrunoAssis @KarenKoritiak o devorador de mundos acordou as #{Time.now.to_s}"`
+      else
+        puts "Não tem :("
+      end
+
+      sleep 20.0
     end
-
-    sleep 20.0
+  rescue
+    puts $!, $@
+    `t update "@BrunoAssis @KarenKoritiak às #{Time.now.to_s}, um super vilão apareceu."`
   end
-rescue
-  puts $!, $@
-  `t update "@BrunoAssis @KarenKoritiak às #{Time.now.to_s}, um super vilão apareceu."`
+  sleep 60.0
 end
